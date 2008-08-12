d6666be292bd6cb27239c793c1dc7ccde4699f15
Evan Goodwin <evan@assho.(none)>
2008-08-12 02:30:32 -0700

    Adding interrupt code

--- main.c
+++ main.c
@@ -6,19 +6,60 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define bool uint8_t
+#define true 1
+#define false 0
+
+static volatile uint8_t intrCount;
+static volatile bool intrFlag;
 
 int main(void)
 {
-    /* insert your hardware initialization here */
-    DDRB = DDB1 | DDRB;
+    intrCount = 0;
+    intrFlag = false;
+
+    cli(); 
+    /* timer0 prescaler = clk/1042 -> 11059200Hz / 1024 = 10800Hz per increment */
+    /* overflow every 256 ticks = 42.1875Hz = 23.7037ms an interrupt */
+    TCCR0B |= _BV(CS00) | _BV(CS02);
+    TCNT0 = 0;
+    TIMSK0 |= _BV(TOIE0); 
+ //   TCCR0A = 0x00;//_BV(WGM01);                //Set CTC Mode disabling the output
+ //   TCCR0B = _BV(CS02) | _BV(CS00);     //1024 prescaler
+ //   OCR0A  = 0xff;                      //essentially overflow
+ //   TCNT0=0x00;
+ //   TIMSK0 = _BV(TOIE0);               //enable OC interupt
+////_BV(OCIE0A) | _BV(TOIE0);               //enable OC interupt
+
+    DDRB  =  _BV(DDB1);
+  //  DDRB &= ~DDB1;
+  //  PORTB = _BV(PB1);
+
+    sei();
+
+   /* insert your hardware initialization here */
     for( ; ; ){
-        /* insert your main loop code here */
-        PINB |= 1 << PB1;
-//		PORTB = 0xFF;
-        _delay_ms(100);
-		PINB = 0x00;
-//        PORTB = PB1 | PORTB;
-//        _delay_ms(1000);
+ 
+        if(intrFlag == true){
+            intrFlag = false;
+           // PORTB ^= _BV(PB1);
+           //PORTB = _BV(PB1);
+        }
     }
     return 0;   /* never reached */
+}
+
+ISR(TIMER0_OVF_vect)
+{
+    if (intrCount >= 2){
+        intrFlag = true;
+        intrCount = 0;
+        PORTB ^= _BV(PB1);
+    }
+    else{
+        intrCount = intrCount + 1;
+    }
+    reti();
 }
